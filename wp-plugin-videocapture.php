bc074aaf3d4befa85e03c0fe50422f3d5db98f2b
mbahrams <mbahrams@gmail.com>
2015-08-22 00:42:48 +1000

    Update wp-plugin-videocapture.php

--- wp-plugin-videocapture.php
+++ wp-plugin-videocapture.php
@@ -25,7 +25,35 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-
+if(!class_exists('WP_Plugin_Template'))
+{
+    class WP_Plugin_Template
+    {
+        /**
+         * Construct the plugin object
+         */
+        public function __construct()
+        {
+            // register actions
+        } // END public function __construct
+    
+        /**
+         * Activate the plugin
+         */
+        public static function activate()
+        {
+            // Do nothing
+        } // END public static function activate
+    
+        /**
+         * Deactivate the plugin
+         */     
+        public static function deactivate()
+        {
+            // Do nothing
+        } // END public static function deactivate
+    } // END class WP_Plugin_Template
+} // END if(!class_exists('WP_Plugin_Template'))
 
 
 
